bd8f4368d10c1ebc96f16af96e62a80821a20ead
Leandro Alves Santos <lealsdev@gmail.com>
2022-06-02 23:03:05 +0100

    API's template and User's entity created

---
+++ src/VirtualMatch.Api/VirtualMatch.Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualMatch.Entities;
+
+namespace VirtualMatch.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/VirtualMatch.Api/VirtualMatch.Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualMatch.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
