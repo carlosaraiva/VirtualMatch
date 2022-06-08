acd8a239e4aab0a7ecbfa3cc7826cc4683398497
Leandro Alves Santos <lealsdev@gmail.com>
2022-06-08 22:54:01 +0100

    User entity complemented and photo entity created

--- src/VirtualMatch.Api/VirtualMatch.Entities/Database/User.cs
+++ src/VirtualMatch.Api/VirtualMatch.Entities/Database/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VirtualMatch.Entities.Database
 {
@@ -11,6 +12,39 @@ namespace VirtualMatch.Entities.Database
         public byte[] Password { get; set; }
 
         public byte[] Salt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+
+            if (DateOfBirth.Date > today.AddYears(-age)) 
+                age--;
+
+            return age;
+        }
 
     }
 }
