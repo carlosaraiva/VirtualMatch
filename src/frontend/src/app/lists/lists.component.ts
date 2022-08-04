1a2c8674e83915df48e220bcfda934f2836676e5
Leandro Alves Santos <lealsdev@gmail.com>
2022-08-04 20:49:54 +0100

    List of likes implemented

--- src/frontend/src/app/lists/lists.component.ts
+++ src/frontend/src/app/lists/lists.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../__models/member';
+import { Pagination } from '../__models/pagination';
+import { MembersService } from '../__services/members.service';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  members: Partial<Member[]>;
+  predicate = 'liked';
+  pageNumber = 1;
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadLikes();
+  }
+  
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe(response => {
+      this.members = response;
+    })
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadLikes();
   }
 
 }
