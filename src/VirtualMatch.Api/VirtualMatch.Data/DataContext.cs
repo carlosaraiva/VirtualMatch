8b59b73dec09f8ec3e2d8f52a011dfb6b76af616
Leandro Alves Santos <lealsdev@gmail.com>
2022-10-05 22:23:23 +0100

    cnn str

--- src/VirtualMatch.Api/VirtualMatch.Data/DataContext.cs
+++ src/VirtualMatch.Api/VirtualMatch.Data/DataContext.cs
@@ -34,7 +34,7 @@ namespace VirtualMatch.Data
                 else
                 {
                     // Use connection string provided at runtime by Heroku.
-                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    /*var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                     // Parse connection URL to connection string for Npgsql
                     connUrl = connUrl.Replace("postgres://", string.Empty);
@@ -47,7 +47,9 @@ namespace VirtualMatch.Data
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";*/
+
+                    connStr = "Server=ec2-107-23-76-12.compute-1.amazonaws.com;Port=5432;User Id=kzegiepnzondsv;Password=7a9aa200b263dcc81471deb34c81cb26133df6600aa521df514f757094dfb879;Database=de6q2g3fl6rql5;SSL Mode=Require;TrustServerCertificate=True";
                 }
 
                 // Whether the connection string came from the local development configuration file
